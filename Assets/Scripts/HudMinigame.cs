650448ae0ff529107ae22aeb1d3f63c6bc3e18bc
hugo-gonzalez <hugo.gonzalez@binmatter.com>
2023-05-07 20:42:12 -0600

    initial commit

---
+++ Assets/Scripts/HudMinigame.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// using tmpro;
+
+
+///<summary>
+///
+///</summary>
+public class HudMinigame : MonoBehaviour {
+	///<summary>
+	///
+	///</summary>
+
+}
